90f7fde0bc0a89d1c3d06e07d543fd56a30e5501
Adrian Serrano <adriansb3105@gmail.com>
2018-06-26 10:33:00 -0600

    Date output modified, reports added

--- src/app/my-bills/my-bills.component.ts
+++ src/app/my-bills/my-bills.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { LogicService } from '../logic.service';
 import { Bill } from '../model/Bill.model';
+import * as jsPDF from 'jspdf';
 
 @Component({
 	selector: 'app-my-bills',
@@ -22,5 +23,25 @@ export class MyBillsComponent implements OnInit {
 
 	getBills(): Bill[]{
 		return this.bills;
+	}
+
+	@ViewChild('content') content: ElementRef;
+	downloadPDF() {
+		let doc = new jsPDF();
+
+		let specialElementHandlers = {
+			'#editor': function (element, renderer) {
+				return true;
+			}
+		};
+
+		let content = this.content.nativeElement;
+
+		doc.fromHTML(content.innerHTML, 15, 15, {
+			'width': 190,
+			'elementHandlers': specialElementHandlers
+		});
+
+		doc.save('Bills.pdf');
 	}
 }
